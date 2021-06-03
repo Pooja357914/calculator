3429780dc3525316dd58b083ea0e8606ac4ca785
Pooja357914 <71167983+Pooja357914@users.noreply.github.com>
2021-06-03 19:47:24 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,16 @@
+a =int(input("Enter the first number:"))
+b =int(input("Enter the second number:"))
+operation =int(input("enter the number from 1-5(1for +,2 for -,3 for *,4for /):"))
+if operation == 1 :
+     print(a+b)
+elif operation == 2 :
+     print(a-b)
+  
+elif operation == 3 :
+     print(a*b)
+     
+elif operation == 4 :
+    print(a/b)
+else:
+    print("Invalid input")
+
