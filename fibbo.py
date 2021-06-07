c889595d05463c2439693389d8c2989d653d3aa6
Pooja thukral <poojathukral10836@gmail.com>
2021-06-07 18:11:06 +0530

    Add files via upload

---
+++ fibbo.py
@@ -0,0 +1,23 @@
+i=0
+j=1
+a=int(input("Enter the range:"))
+for b in range(0, a):
+           if(b <= 1):
+                      Next = b
+           else:
+                      Next =i+j
+                      i = j
+                      j = Next
+           print(Next)
+
+    
+    
+    
+    
+  
+
+
+
+
+    
+ 
